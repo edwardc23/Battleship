db44e4a47c7c1d83a528cbe5c28fc326e490ab7c
Edward Cochran <edwardcochran23@gmail.com>
2020-03-18 18:18:46 -0400

    2nd commit

--- src/Battleship.java
+++ src/Battleship.java
@@ -2,34 +2,78 @@ import java.util.Scanner;
 
 public class Battleship {
     static Scanner in = new Scanner(System.in);
-   static String player1,player2;
-   static Grid grid=new Grid();
+  static boolean gameOver=false;
+  static int i =0;
+  static int turns=0;
     public static void main(String[] args) {
         System.out.println("Battleship Multiplayer");
         System.out.println("Enter Player 1 name: ");
         Player player1=new Player(in.next());
         System.out.println("Enter Player 2 name: ");
         Player player2=new Player(in.next());
+while(!gameOver) {
+    if(i==0){
+//    player1.addPieces("Carrier");
+//    player1.gridP.printBoard();
+//    player1.addPieces("Battleship");
+//    player1.gridP.printBoard();
+//    player1.addPieces("Destroyer");
+//    player1.gridP.printBoard();
+//    player1.addPieces("Submarine");
+//    player1.gridP.printBoard();
+//    player1.addPieces("PatrolBoat");
+//    player1.gridP.printBoard();
 
-player1.addPieces("Carrier");
-player1.gridP.printBoard();
-player1.addPieces("Battleship");
-player1.gridP.printBoard();
-player1.addPieces("Destroyer");
-player1.gridP.printBoard();
-player1.addPieces("Submarine");
-player1.gridP.printBoard();
-player1.addPieces("Patrol Boat");
-player1.gridP.printBoard();
-//player2.addPieces();
-        int Carrier=5;
-        int Battleship=4;
-        int Destroyer=3;
-        int Submarine=2;
-        int PatrolBoat=2;
+    player2.addPieces("Carrier");
+    player2.gridP.printBoard();
+    player2.addPieces("Battleship");
+    player2.gridP.printBoard();
+    player2.addPieces("Destroyer");
+    player2.gridP.printBoard();
+    player2.addPieces("Submarine");
+    player2.gridP.printBoard();
+    player2.addPieces("PatrolBoat");
+    player2.gridP.printBoard();
+    i++;}
+    else if(turns%2==0){
+        System.out.println(player1.name+" ,Enter the coordinates of your guess: ");
+        String p1=in.next();
+        player1.guessIt(player2,p1);
+        player2.gridP.printBoard();
+
+   }
+//    else
+//    {
+//        System.out.println(player2.name+" ,Enter the coordinates of your guess: ");
+//        String p1=in.next();
+//        player1.guessIt(player2,p1);
+//        player1.gridP.printBoard();
+//        turns++;
+//    }
+    if(player1.won||player2.won)
+    {
+        System.out.println("Play again Y or N");
+        String ans=in.next();
+        if(ans.equals("N"))
+        {
+        gameOver=true;
+        break;
+        }
+        else if(ans.equals("Y"))
+        {
+            player1.won=false;
+            player2.won=false;
+                i=0;
+                turns=0;
+            System.out.println("Enter Player 1 name: ");
+             player1=new Player(in.next());
+            System.out.println("Enter Player 2 name: ");
+             player2=new Player(in.next());
+        }
+
+    }
+}
 
 
-//         player1.gridP.printBoard(); System.out.println();
-//        player2.gridP.printBoard();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -8,6 +8,12 @@ public class Player{
     Grid gridP=new Grid();
     String [][] Board=new String[10][10];
     String typeOfShip;
+    int c=5;
+    int b=4;
+    int d=3;
+    int s=2;
+    int p=2;
+    boolean won=false;
 
 
     ArrayList<Ship> g=new ArrayList<>();
@@ -24,21 +30,21 @@ public class Player{
     String e="";
         if(type.equals("Carrier")){
             System.out.println(name+" ,please enter the coordinates for your ships");
-            System.out.println("Enter the coordinates for the"+ gridP.nCarrier+": ");
+            System.out.println("Enter the coordinates for the "+ gridP.nCarrier+": ");
         sName=gridP.nCarrier;
         space=gridP.Carrier;
         e="C";
         }
-       else if(type.equals("BattleShip")){
+       else if(type.equals("Battleship")){
             System.out.println(name+" ,please enter the coordinates for your ships");
-            System.out.println("Enter the coordinates for the"+ gridP.nBattleship+": ");
+            System.out.println("Enter the coordinates for the "+ gridP.nBattleship+": ");
             sName=gridP.nBattleship;
             space=gridP.Battleship;
             e="B";
        }
         else if(type.equals("Destroyer")){
             System.out.println(name+" ,please enter the coordinates for your ships");
-            System.out.println("Enter the coordinates for the"+ gridP.nDestroyer+": ");
+            System.out.println("Enter the coordinates for the "+ gridP.nDestroyer+": ");
             sName=gridP.nDestroyer;
             space=gridP.Destroyer;
             e="D";
@@ -46,14 +52,14 @@ public class Player{
         }
         else if(type.equals("Submarine")){
             System.out.println(name+" ,please enter the coordinates for your ships");
-            System.out.println("Enter the coordinates for the"+ gridP.nSubmarine+": ");
+            System.out.println("Enter the coordinates for the "+ gridP.nSubmarine+": ");
             sName=gridP.nSubmarine;
             space=gridP.Submarine;
             e="S";
         }
         else if(type.equals("PatrolBoat")){
             System.out.println(name+" ,please enter the coordinates for your ships");
-            System.out.println("Enter the coordinates for the"+ gridP.nPatrolBoat+": ");
+            System.out.println("Enter the coordinates for the "+ gridP.nPatrolBoat+": ");
             sName=gridP.nPatrolBoat;
             space=gridP.PatrolBoat;
             e="P";
@@ -66,7 +72,7 @@ public class Player{
       String pl="not empty";
          for(char d:s.toCharArray())
          {
-             if(d!='('&&d!=')'&&d!=',')
+             if(d!='('&&d!=')'&&d!=','&&Character.isDigit(d))
              {
                  if(t==0)
                  {
@@ -102,13 +108,22 @@ public class Player{
 
          }
          g.add(new Ship(x,y,pl,space,sName));
-        for(int a=0,c=x,f=y;a<=space;a++)
+        for(int a=0,c=x,f=y;a<space;a++)
         {
             if(pl.equals("v"))
             {
-                if(x+space<=9){
+                if(x+space<=10){
+                    if(gridP.board[c][y]=="~"){
                 gridP.board[c][y]=e;
                 c++;}
+                else{
+                        System.out.println("Can't place ship here New Coordinates: ");
+                    g.remove(g.size()-1);
+                    addPieces(type);
+                    break;
+                    }
+                }
+
                 else{
                     System.out.println("Can't place ship here New Coordinates: ");
                     g.remove(g.size()-1);
@@ -118,18 +133,108 @@ public class Player{
             }
             else if(pl.equals("h"))
             {
-                if(y+space<=9){
-                gridP.board[x][f]=e;
-                f++;}
+                if(y+space<=10){
+                    if(gridP.board[x][f]=="~"){
+                        gridP.board[x][f]=e;
+                        f++;}
+                    else{
+                        System.out.println("Can't place ship here New Coordinates: ");
+                        g.remove(g.size()-1);
+                        addPieces(type);
+                        break;
+                    }
+                }
                 else{
                     System.out.println("Can't place ship here New Coordinates: ");
                     g.remove(g.size()-1);
                     addPieces(type);
-                    break;
+                    break;}
+            }
+        }
+
+    }
+    public void guessIt(Player opponent,String guess)
+    {
+        String[][] oBoard=opponent.gridP.board;
+
+        int t=0;
+        int x=0,y=0;
+
+        for(char d:guess.toCharArray())
+        {
+            if(d!='('&&d!=')'&&d!=','&&Character.isDigit(d))
+            {
+                if(t==0)
+                {
+                    x=Integer.parseInt(String.valueOf(d));
+                    t++;
+                }
+                else if(t==1)
+                {
+                    y=Integer.parseInt(String.valueOf(d));
 
                 }
             }
         }
+        if(oBoard[x][y]!="~")
+        {
+            switch (oBoard[x][y]){
+                case "C":
+                    c--;
+                    if(c==0)
+                    {
+                        System.out.println("You have sunk the Carrier");
 
+                    }
+                    break;
+                case "B":
+                    b--;
+                    if(b==0)
+                    {
+                        System.out.println("You have sunk the Battleship");
+                    }
+                    break;
+                case "D":
+                    d--;
+                    if(d==0)
+                    {
+                        System.out.println("You have sunk the Destroyer");
+
+                    }
+                    break;
+                case "S":
+                    s--;
+                    if(s==0)
+                    {
+                        System.out.println("You have sunk the Submarine");
+
+                    }
+                    break;
+                case "P":
+                    p--;
+                    if(p==0)
+                    {
+                        System.out.println("You have sunk the Patrol Boat");
+
+                    }
+
+            }
+            oBoard[x][y]="X";
+        }
+        else if(oBoard[x][y]=="X")
+        {
+            System.out.println("Already hit Choose again");
+            guessIt(opponent, in.next());
+        }
+        else
+        {
+            System.out.println("Missed");
+        }
+        if (c==0&&b==0&&d==0&&s==0&&p==0)
+        {
+            System.out.println(name+" ,you have won");
+            won=true;
+
+        }
     }
 }
